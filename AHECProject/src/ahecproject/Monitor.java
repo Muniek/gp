ff606ffee9736b9afb7537de4c6652459ad9d9b1
Tiago <Tiago@Tiago-TOSH>
2014-03-11 20:22:31 +0000

    Monitor structure defined. Still missing the thread handling

--- AHECProject/src/ahecproject/Monitor.java
+++ AHECProject/src/ahecproject/Monitor.java
@@ -3,13 +3,72 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ahecproject;
+
+import java.sql.*;
 
 /**
  *
  * @author Dsiefus
  */
 public class Monitor {
+    Monitor() {
+        //TODO
+    }
     
+    public static boolean postShapeParameters(double r, double t, double theta) {
+        //TODO
+        return true;
+    }
+
+    public static boolean postDrag() {
+        //TODO
+        return true;
+    }
+
+    public static boolean postLift() {
+        //TODO
+        return true;
+    }
+
+    public static boolean logUser(String username, String pass) {
+        //TODO
+        return true;
+    }
+
+    public static double getRecoveredR() {
+        //TODO
+        return 1.0;
+    }
+
+    public static double getRecoveredT() {
+        //TODO
+        return 1.0;
+    }
+
+    public static double getRecoveredTetha() {
+        //TODO
+        return 1.0;
+    }
+    
+    public static double getRecoveredDrag() {
+        //TODO
+        return 1.0;
+    }
+
+    public static double getRecoveredLift() {
+        //TODO
+        return 1.0;
+    }
+    
+    public static double getBestDragLift() {
+        //TODO
+        return 1.0;
+    }
+    
+    public static boolean resetDB(){
+        //TODO
+        return true;
+    }
 }
+
